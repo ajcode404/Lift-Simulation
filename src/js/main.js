2a02c759086b94c188cf7915f4914de0ed140939
Akash Jain <ajcodes.tech@gmail.com>
2024-11-16 20:08:55 +0530

    lift render

--- src/js/main.js
+++ src/js/main.js
@@ -12,12 +12,10 @@ function createDataStore(config) {
     }
     const lifts = []
     for (let index = 0; index < config.numberOfLifts; index++) {
-        lifts.push({ 'id': `lift-id-${index}`, 'curr': 0 })
+        lifts.push({ 'id': `lift-id-${index}`, 'currLoc': config.numberOfFloors - 1 })
     }
     return { floors, lifts }
 }
-
-const d = createDataStore(10, 10)
 
 function refreshConfig() {
     const numberOfFloors = document.getElementById('config-floor').value;
@@ -37,6 +35,57 @@ function refreshConfig() {
     return { numberOfFloors, numberOfLifts }
 }
 
+function renderFloors(floors) {
+    for (let index = 0; index < floors.length; index++) {
+        const floor = floors[index];
+        // lift buttons
+        const up = document.createElement('button')
+        up.setAttribute('id', floor.up)
+        up.innerHTML = 'Up'
+        const down = document.createElement('button')
+        down.innerHTML = 'Down'
+        down.setAttribute('id', floor.down)
+
+        // button div
+        const fc = document.createElement('div')
+        fc.setAttribute('class', 'floor-component')
+        fc.appendChild(up)
+        fc.appendChild(down)
+
+        // parent div
+        const f = document.createElement('div')
+        f.setAttribute('id', `floor-${index}`)
+        f.setAttribute('class', 'floor')
+        f.appendChild(fc)
+
+        // append to canvas
+        const canvas = document.getElementById('canvas')
+        canvas.appendChild(f)
+    }
+}
+
+function renderLifts(lifts) {
+    for (let index = 0; index < lifts.length; index++) {
+        const lift = lifts[index];    
+        // lift doors
+        const ldl = document.createElement('div')
+        ldl.classList.add('lift-door', 'lift')
+
+        const ldr = document.createElement('div')
+        ldr.classList.add('lift-door', 'right')
+
+        // lift box
+        const fcl = document.createElement('div');
+        fcl.classList.add('floor-component', 'lift')
+        fcl.appendChild(ldl)
+        fcl.appendChild(ldr)
+        
+        // append to the floor
+        const floor = document.getElementById(`floor-${lift.currLoc}`)
+        floor.appendChild(fcl)
+    }
+}
+
 function createFloor() {
     const canvas = document.getElementById('canvas');
 }
@@ -44,19 +93,21 @@ function createFloor() {
 document.getElementById('config-submit-btn').addEventListener('click', () => {
     const configs = refreshConfig()
     const datastore = createDataStore(configs);
+    renderFloors(datastore.floors)
+    renderLifts(datastore.lifts)
     console.log(datastore)
 })
 
-document.addEventListener('DOMContentLoaded', () => {
 
+document.addEventListener('DOMContentLoaded', () => {
     const up = document.getElementById('up1');
     const down = document.getElementById('down1');
-
     up.addEventListener('click', () => {
-        lift.classList.add('open'); // Add the class to open doors
+        lift.classList.add('open');
     });
 
     down.addEventListener('click', () => {
-        lift.classList.remove('open'); // Remove the class to close doors
+        lift.classList.remove('open');
     });
 });
+
