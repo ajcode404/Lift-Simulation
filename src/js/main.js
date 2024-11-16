51e89ec977f6f42c326628349bdeea1cf74d13e8
Akash Jain <138494702+ajcode404@users.noreply.github.com>
2024-11-16 19:40:08 +0530

    Lift config (#1)
    * lift animation
    
    * animation for lift
    
    * config refactor

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,21 @@ const FLOOR_MAX_LIMIT = 100
 const LIFT_MIN_LIMIT = 1
 const LIFT_MAX_LIMIT = 10
 
+
+function createDataStore(config) {
+    const floors = []
+    for (let index = 0; index < config.numberOfFloors; index++) {
+        floors.push({ 'up': `up-${index}`, 'down': `down-${index}` })
+    }
+    const lifts = []
+    for (let index = 0; index < config.numberOfLifts; index++) {
+        lifts.push({ 'id': `lift-id-${index}`, 'curr': 0 })
+    }
+    return { floors, lifts }
+}
+
+const d = createDataStore(10, 10)
+
 function refreshConfig() {
     const numberOfFloors = document.getElementById('config-floor').value;
     const numberOfLifts = document.getElementById('config-lift').value;
@@ -19,9 +34,29 @@ function refreshConfig() {
         window.alert(`floor limit should be in the range of ${LIFT_MIN_LIMIT} to ${LIFT_MAX_LIMIT}`);
         return;
     }
-    console.log(numberOfFloors, numberOfLifts);
+    return { numberOfFloors, numberOfLifts }
+}
+
+function createFloor() {
+    const canvas = document.getElementById('canvas');
 }
 
 document.getElementById('config-submit-btn').addEventListener('click', () => {
-    refreshConfig()
+    const configs = refreshConfig()
+    const datastore = createDataStore(configs);
+    console.log(datastore)
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const up = document.getElementById('up1');
+    const down = document.getElementById('down1');
+
+    up.addEventListener('click', () => {
+        lift.classList.add('open'); // Add the class to open doors
+    });
+
+    down.addEventListener('click', () => {
+        lift.classList.remove('open'); // Remove the class to close doors
+    });
+});
